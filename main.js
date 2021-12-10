176571ff46e07c6f7c8676d496c8bcba80d9c4ef
Tzu1 <44744600+Tzu101@users.noreply.github.com>
2021-12-10 21:26:52 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { Matrix } from "./Matrix.js"
 import { SceneRender } from "./SceneReader.js"
-import { buildInModel } from "/model.js"
+import { buildInModel } from "./model.js"
 
 
 // Global variables
